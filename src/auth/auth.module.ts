3c6a48b682013ae873f4e2d8536236917a280bf5
majidln <majidlotfinia12@gmail.com>
2020-08-28 19:16:28 +0430

    fix auth problem

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,5 +1,6 @@
 
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './local.strategy';
 import { JwtStrategy } from './jwt.strategy';
@@ -11,12 +12,16 @@ import { JwtModule } from '@nestjs/jwt';
   imports: [
     UsersModule,
     PassportModule,
-    JwtModule.register({
-      secret: process.env.JWT_SECRET + "",
-      signOptions: { expiresIn: '3600s' },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async () => ({
+        secret: process.env.JWT_SECRET,
+        signOptions: { expiresIn: '3600s' },
+      }),
     }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
   exports: [AuthService],
 })
+
 export class AuthModule {}
